2187c29e1c914b32fb019e8ef2e3f13069fc3410
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-12-17 20:50:21 +0300

    bug düzeltildi

--- NoteProject.UI/AdminScreen.Designer.cs
+++ NoteProject.UI/AdminScreen.Designer.cs
@@ -63,6 +63,7 @@
             listView1.TabIndex = 6;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
             // 
             // columnHeader1
             // 
